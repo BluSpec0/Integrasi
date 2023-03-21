2df08b4771d3d9cfcbc7337905be875908740393
Snorrlaxx <rivo.putra@suretybond.co.id>
2023-03-21 17:12:22 +0700

    ganti background jadi biru

--- src/components/ExampleLayout.tsx
+++ src/components/ExampleLayout.tsx
@@ -6,7 +6,7 @@ const ExampleLayout = ({ children }: any) => {
       <div className="fixed z-50 w-full bg-white h-[64px] p-5 border border-b">
         Header
       </div>
-      <div className="pt-[88px] pb-4 px-2 md:px-5">{children}</div>
+      <div className="pt-[88px] pb-4 px-2 md:px-5 bg-blue-300">{children}</div>
       <div className="w-full bg-gray-50 h-[100px] pb-[48px] md:pb-0 px-2 md:px-5">
         Footer
       </div>
